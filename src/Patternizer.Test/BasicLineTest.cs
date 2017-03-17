aaa2c4be8c4af14761648bc7866644c130f121d8
Johan Karlsson <johan.kson@gmail.com>
2017-03-17 08:35:24 +0100

    Downgraded nUnit to 2.x

---
+++ src/Patternizer.Test/BasicLineTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Patternizer;
+
+namespace Patternizer.Test
+{
+	[TestFixture]
+	public class BasicLineTest
+	{
+
+		[Test]
+		public void LineDownTest()
+		{
+			// Arrange
+			var evaluator = new PatternEvaluator();
+			var path = Path.Parse("10,300 -> 10,200");
+
+			// Act
+			evaluator.Add("linedown").When(e=>e.MovesDown());
+			var result = evaluator.Evaluate(path);
+
+			// Assert
+			Assert.IsTrue(result.IsValid);
+			Assert.AreEqual("linedown", result.Key);
+		}
+	}
+}
